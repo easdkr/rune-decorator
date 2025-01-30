b5e2d05fe8891387109e4b1d51dd1f3b1b8e6217
June <easdkr@daum.net>
2025-01-30 18:29:36 +0900

    [test] 디버깅

--- src/client/common/decorators/rune-page.decorator.ts
+++ src/client/common/decorators/rune-page.decorator.ts
@@ -6,7 +6,8 @@ export const PAGE_METADATA = Symbol("__rune:page__");
 export function RunePage(key?: string) {
   return (target: ClassConstructor<Page<any>>) => {
     const runeKey = key || target.name;
-
+    console.log("runekey", runeKey);
+    console.log("target", target);
     const newConstructor = function (...args: any[]) {
       const instance = new target(...args);
       instance.key = runeKey;
