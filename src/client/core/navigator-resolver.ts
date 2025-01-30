b0d337303eb4aeb35323dac92057ccebdbababa8
June <easdkr@daum.net>
2025-01-30 18:06:54 +0900

    [test] 디버깅

--- src/client/core/navigator-resolver.ts
+++ src/client/core/navigator-resolver.ts
@@ -11,26 +11,27 @@ export class NavigatorResolver {
   // TODO: 코드 정리할 것
   async resolve(navigator: any) {
     // 정적으로 등록된 페이지를 컨테이너에 등록 및 초기화
-    pipe(
-      (Reflect.getMetadata("pages", navigator) || []) as any[],
-      forEach((page) =>
-        this.container.set(Reflect.getMetadata(PAGE_METADATA, page), page)
-      )
-    );
+    // pipe(
+    //   (Reflect.getMetadata("pages", navigator) || []) as any[],
+    //   forEach((page) =>
+    //     this.container.set(Reflect.getMetadata(PAGE_METADATA, page), page)
+    //   )
+    // );
 
-    hydrate(this.container.resolveAll() as Record<string, any>);
+    // hydrate(this.container.resolveAll() as Record<string, any>);
 
     // dynamic import로 등록된 페이지를 바로 hydrate
     const content = JSON.parse(
       document.querySelector("script.__RUNE_DATA__")?.textContent ?? "{}"
     );
-
+    console.log(content);
     const dynamic = Reflect.getMetadata("dynamic", navigator) as Record<
       string,
       DynamicImport<Page<any>>
     >;
-
+    console.log(dynamic);
     const target = await dynamic[content.key]();
+    console.log(target);
     hydrate({
       [content.key]: target as any,
     });
