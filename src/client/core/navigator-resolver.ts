a327e583d6dd1d250f1d0e75ab79e536fe474f6a
June <easdkr@daum.net>
2025-01-30 22:07:11 +0900

    [fix] 정적 페이지 하이드레이션 코드 제거

--- src/client/core/navigator-resolver.ts
+++ src/client/core/navigator-resolver.ts
@@ -8,7 +8,7 @@ import { Page } from "rune-ts";
 export class NavigatorResolver {
   constructor(private readonly container: RunePageContainer) {}
 
-  // TODO: 코드 정리할 것
+  // TODO: 코드 정리할 것, 정적 페이지 등록을 지원하지 않을까 고민...
   async resolve(navigator: any) {
     // 정적으로 등록된 페이지를 컨테이너에 등록 및 초기화
     // pipe(
@@ -30,10 +30,7 @@ export class NavigatorResolver {
     >;
 
     const target = await dynamic[content.key]();
-    console.log({
-      key: content.key,
-      target: target,
-    });
+
     hydrate({
       [content.key]: target as any,
     });
